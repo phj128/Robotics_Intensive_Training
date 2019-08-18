ef2b7868dbf387ad02c7eaaf8ac340d957e65849
phj128 <604608501@qq.com>
2019-08-18 16:27:16 +0800

    update receive

--- receive.py
+++ receive.py
@@ -8,11 +8,28 @@ class Receive():
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(("127.0.0.1", 23333))
-        self.robot_info = [0, 0, 0, 0, 0, 0]
-        self.ball_info = [0, 0, 0, 0]
+        self.robot_info = {'x': 0, 'y': 0, 'ori': 0, 'vx': 0, 'vy': 0, 'w': 0, 'ax': 0, 'ay': 0,
+                           'r_x': 0, 'r_y': 0, 'r_ori': 0, 'r_vx': 0, 'r_vy': 0, 'r_w': 0}
 
 
-    def Get_Info(self, color, robotID):
+    def convert_info(self, robot):
+        self.robot_info['x'] = robot.x
+        self.robot_info['y'] = robot.y
+        self.robot_info['ori'] = robot.orientation
+        self.robot_info['vx'] = robot.vel_x
+        self.robot_info['vy'] = robot.vel_y
+        self.robot_info['w'] = robot.rotate_vel
+        self.robot_info['ax'] = robot.accelerate_x
+        self.robot_info['ay'] = robot.accelerate_y
+        self.robot_info['r_x'] = robot.raw_x
+        self.robot_info['r_y'] = robot.raw_y
+        self.robot_info['r_ori'] = robot.raw_orientation
+        self.robot_info['r_vx'] = robot.raw_vel_x
+        self.robot_info['r_vy'] = robot.raw_vel_y
+        self.robot_info['r_w'] = robot.raw_rotate_vel
+
+
+    def get_info(self, color, id):
         data, address = self.sock.recvfrom(4096)
         sleep(0.001)
 
@@ -21,40 +38,23 @@ class Receive():
 
         robots_yellow = package.robots_yellow
         robots_blue = package.robots_blue
-        import ipdb;
-        ipdb.set_trace()
-        # 0 is yellow
-        if color is 0:
+        # import ipdb;ipdb.set_trace()
+
+        if color == 'yellow':
             for robot in robots_yellow:
-                if robot.robot_id == robotID:
-                    self.robot_info[0] = robot.x
-                    self.robot_info[1] = robot.y
-                    self.robot_info[2] = robot.orientation
-                    self.robot_info[3] = robot.vel_x
-                    self.robot_info[4] = robot.vel_y
-                    self.robot_info[5] = robot.rotate_vel
+                if robot.robot_id == id:
+                    self.convert_info(robot)
+
         else:
             for robot in robots_blue:
-                if robot.robot_id == robotID:
-                    self.robot_info[0] = robot.x
-                    self.robot_info[1] = robot.y
-                    self.robot_info[2] = robot.orientation
-                    self.robot_info[3] = robot.vel_x
-                    self.robot_info[4] = robot.vel_y
-                    self.robot_info[5] = robot.rotate_vel
-
-        balls = package.balls
-        self.ball_info[0] = balls.vel_x
-        self.ball_info[1] = balls.vel_y
-        self.ball_info[2] = balls.x
-        self.ball_info[3] = balls.y
+                if robot.robot_id == id:
+                    self.convert_info(robot)
 
 
 if __name__ == "__main__":
     while True:
         receive = Receive()
-        receive.Get_Info('yellow', 0)
+        receive.get_info('yellow', 0)
         print(receive.robot_info)
-        print(receive.ball_info)
         sleep(1)
 
