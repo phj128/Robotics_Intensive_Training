b40f29af770836e36da1ed347899829defbcd1bc
kyleleey <45925255+kyleleey@users.noreply.github.com>
2019-08-18 20:12:55 +0800

    Add files via upload

--- send.py
+++ send.py
@@ -1,56 +1,67 @@
 import socket
 from time import sleep
-import grSim_Packet_pb2 as grSim_Pkg
+import proto.zss_debug_pb2 as control
 
-class Send:
+class Send():
     def __init__(self):
-        self.address = ('127.0.0.1',20011)
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(("127.0.0.1", 50001))
+        self.robot_status_dic = {'robot_id': 0, 'infrared': False, 'flat_kick': False, 'chip_kick': False}
+        self.robot_cmd_dic = {'delay': 1, 'robot_id': 0, 'velocity_x': 0, 'velocity_y': 0, 'velocity_r': 0,
+                          'kick': False,  'power': 1, 'dribbler_spin': 1, 'current_angle': 0, 'target_angle': 0}
 
-    def Send_Command(self, robot_ID=0, veltangent=0, velnormal=0, velangular=0):
-        package = grSim_Pkg.grSim_Packet()
-        commands = package.commands
+    def send_msg(self, id_we_send, v_x, v_y, v_r):
 
-        commands.timestamp = 0
-        commands.isteamyellow = True
-        robot_commands = commands.robot_commands.add()
+        robot_status = control.Robot_Status()
+        robotid = 0
+        for robotid in range(8):
+            robot_status.robot_id = robotid
+            robot_status.infrared = self.robot_status_dic['infrared']
+            robot_status.flat_kick = self.robot_status_dic['flat_kick']
+            robot_status.chip_kick = self.robot_status_dic['chip_kick']
+            senddata = control.Robots_Status().SerializeToString()
+            self.sock.sendto(senddata,('127.0.0.1',50001))
 
-        robot_commands.id = robot_ID
-        robot_commands.kickspeedx = 0
-        robot_commands.kickspeedz = 0
-        robot_commands.veltangent = veltangent
-        robot_commands.velnormal = velnormal
-        robot_commands.velangular = velangular
-        robot_commands.spinner = 0
-        robot_commands.wheelsspeed = False
+            #robotid = robotid + 1
 
-        self.socket.sendto(package.SerializeToString(),self.address)
+        robot_cmd = control.Robot_Command()
+        robotiid = 0
+        for robotiid in range(8):
+            if robotiid == id_we_send:
+                #robot_cmd.delay = self.robot_cmd_dic['delay']
+                robot_cmd.robot_id = robotiid
+                robot_cmd.velocity_x = v_x
+                robot_cmd.velocity_y = v_y
+                robot_cmd.velocity_r = v_r
+                robot_cmd.kick = self.robot_cmd_dic['kick']
+                robot_cmd.power = self.robot_cmd_dic['power']
+                robot_cmd.dribbler_spin = self.robot_cmd_dic['dribbler_spin']
+                robot_cmd.current_angle = self.robot_cmd_dic['current_angle']
+                robot_cmd.target_angle = self.robot_cmd_dic['target_angle']
+            else:
+                #robot_cmd.delay = self.robot_cmd_dic['delay']
+                robot_cmd.robot_id = robotiid
+                robot_cmd.velocity_x = self.robot_cmd_dic['velocity_x']
+                robot_cmd.velocity_y = self.robot_cmd_dic['velocity_y']
+                robot_cmd.velocity_r = self.robot_cmd_dic['velocity_r']
+                robot_cmd.kick = self.robot_cmd_dic['kick']
+                robot_cmd.power = self.robot_cmd_dic['power']
+                robot_cmd.dribbler_spin = self.robot_cmd_dic['dribbler_spin']
+                robot_cmd.current_angle = self.robot_cmd_dic['current_angle']
+                robot_cmd.target_angle = self.robot_cmd_dic['target_angle']
 
-    def Set_Ball(self, x=0, y=0, vx=0, vy=0):
-        package = grSim_Pkg.grSim_Packet()
-        replacement = package.replacement
-
-        ball = replacement.ball
-        ball.x = x
-        ball.y = y
-        ball.vx = vx
-        ball.vy = vy
-
-        self.socket.sendto(package.SerializeToString(),self.address)
-
-    def MoveTo(self, x, y):
+            senddata = control.Robots_Command().SerializeToString()
+            self.sock.sendto(senddata,('127.0.0.1',50001))
+            robotiid = robotiid + 1
 
 
-    def Set_Robot(self, robot_ID=0, x=0, y=0, vx=0, vy=0):
-        package = grSim_Pkg.grSim_Packet()
-        replacement = package.replacement
+if __name__ == "__main__":
+    while True:
+        send = Send()
+        send.send_msg(4,1,2,3)
+        sleep(1)
+'''
 
-        robots = replacement.robots.add()
-        robots.id = robot_ID
-        robots.yellowteam = True
-        robots.x = x
-        robots.y = y
-        robots.vx = vx
-        robots.vy = vy
-
-        self.socket.sendto(package.SerializeToString(),self.address)
+package = control.Robots_Status.robots_status
+print(package)'''
