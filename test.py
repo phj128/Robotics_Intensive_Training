9ad483e5dc3522dece1aa64b01899ddb36cecf5f
phj128 <604608501@qq.com>
2019-08-23 09:09:41 +0800

    thread

---
+++ test.py
@@ -0,0 +1,81 @@
+from message.send import Send
+from message.send_debug import SendDebug
+from message.receive import Receive
+
+from global_planner.myRRT_static import RRT
+from global_planner.myRRTstar import RRT as RRT_STAR
+from global_planner.myRRTmerge import RRT as RRT_MERGE
+from global_planner.A_star import AStar as A_star
+
+from ArtificialPotentialFieldMethod.myAPF import APF
+
+from local_planner.p_control import P_control
+from local_planner.xy_control import XY_control
+from local_planner.xy_near import XY_near
+from local_planner.xy_p import XY_p
+from local_planner.xy_angle import XY_angle
+from local_planner.xy_speed import XY_speed
+
+from run import run
+from run import run_while
+from run import run_line
+from run import run_shrink
+import time
+
+
+def test_0():
+    color = 'blue'
+    robot_id = 0
+    barriers = [['yellow', 0], ['yellow', 1], ['yellow', 2], ['yellow', 3],
+                ['yellow', 4], ['yellow', 5], ['yellow', 6], ['yellow', 7],
+                ['blue', 1], ['blue', 2], ['blue', 3], ['blue', 4],
+                ['blue', 5], ['blue', 6], ['blue', 7]]
+    g_x, g_y = (250, 180)
+    # barriers = [['yellow', 0], ['blue', 1], ['blue', 2], ['blue', 3],
+    #             ['blue', 4], ['blue', 5], ['blue', 6], ['blue', 7]]
+
+    i = 0
+    global_p = RRT_MERGE
+    local_p = XY_speed
+    RUN = run_shrink
+
+    receive = Receive()
+
+    while True:
+        start = time.time()
+        if i % 2 == 0:
+            RUN(color, robot_id, barriers, g_x, g_y, global_p, local_p, receive)
+        else:
+            RUN(color, robot_id, barriers, -g_x, -g_y, global_p, local_p, receive)
+        i += 1
+        end = time.time()
+        print('a circle time:', end - start)
+
+
+def test_5():
+    color = 'blue'
+    robot_id = 5
+    barriers = [['yellow', 0], ['yellow', 1], ['yellow', 2], ['yellow', 3],
+                ['yellow', 4], ['yellow', 5], ['yellow', 6], ['yellow', 7],
+                ['blue', 1], ['blue', 2], ['blue', 3], ['blue', 4],
+                ['blue', 5], ['blue', 6], ['blue', 7]]
+    g_x, g_y = (250, -180)
+    # barriers = [['yellow', 0], ['blue', 1], ['blue', 2], ['blue', 3],
+    #             ['blue', 4], ['blue', 5], ['blue', 6], ['blue', 7]]
+
+    i = 0
+    global_p = RRT_MERGE
+    local_p = XY_speed
+    RUN = run_shrink
+
+    receive = Receive()
+
+    while True:
+        start = time.time()
+        if i % 2 == 0:
+            RUN(color, robot_id, barriers, g_x, g_y, global_p, local_p, receive)
+        else:
+            RUN(color, robot_id, barriers, -g_x, -g_y, global_p, local_p, receive)
+        i += 1
+        end = time.time()
+        print('a circle time:', end - start)
--- thread.py
+++ thread.py
@@ -1,21 +1,10 @@
 import time, threading
+from test import test_0, test_5
 
-x = 1
-def print_x():
-    for _ in range(100):
-        print(x)
-        time.sleep(1)
 
-def change_x():
-    for _ in range(100):
-        global x
-        x += 1
-
-t1 = threading.Thread(target=print_x, name='print')
-t2 = threading.Thread(target=change_x, name='change')
+t1 = threading.Thread(target=test_0, name='blue0')
+t2 = threading.Thread(target=test_5, name='blue5')
 t1.start()
 t2.start()
 t1.join()
 t2.join()
-while True:
-    print('thread %s ended.' % threading.current_thread().name)
